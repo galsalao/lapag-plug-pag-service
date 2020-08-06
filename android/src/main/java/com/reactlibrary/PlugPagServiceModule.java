7aa24833b3c2130e7fa06657ff3807f308446949
Vagner A Saraiva <vagner@vizir.com.br>
2020-08-06 18:35:18 -0300

    returning plugpag version 1.8.5

--- android/src/main/java/com/reactlibrary/PlugPagServiceModule.java
+++ android/src/main/java/com/reactlibrary/PlugPagServiceModule.java
@@ -15,10 +15,10 @@ import java.util.Map;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPag;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagActivationData;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagAppIdentification;
-// import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagCardInfoResult;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagCardInfoResult;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagInitializationResult;
-// import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagNearFieldCardData;
-// import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagNFCResult;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagNearFieldCardData;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagNFCResult;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagPaymentData;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagTransactionResult;
 
@@ -49,7 +49,7 @@ public class PlugPagServiceModule extends ReactContextBaseJavaModule {
             put("TYPE_CREDITO", PlugPag.TYPE_CREDITO);
             put("TYPE_DEBITO", PlugPag.TYPE_DEBITO);
             put("TYPE_VOUCHER", PlugPag.TYPE_VOUCHER);
-            // put("TYPE_QRCODE", PlugPag.TYPE_QRCODE);
+            put("TYPE_QRCODE", PlugPag.TYPE_QRCODE);
 
             put("INSTALLMENT_TYPE_A_VISTA", PlugPag.INSTALLMENT_TYPE_A_VISTA);
             put("INSTALLMENT_TYPE_PARC_VENDEDOR", PlugPag.INSTALLMENT_TYPE_PARC_VENDEDOR);
@@ -197,81 +197,81 @@ public class PlugPagServiceModule extends ReactContextBaseJavaModule {
         }
     }
 
-    // @ReactMethod
-    // public void calculateInstallments(String plugPagId, String saleValue, Callback successCallback, Callback errorCallback) {
-    //     try {
-    //         if (saleValue != null && saleValue != "0") {
-    //             PlugPagWrapper plugPagWrapper = null;
+    @ReactMethod
+    public void calculateInstallments(String plugPagId, String saleValue, Callback successCallback, Callback errorCallback) {
+        try {
+            if (saleValue != null && saleValue != "0") {
+                PlugPagWrapper plugPagWrapper = null;
 
-    //             for (PlugPagWrapper wrapper: plugPags) {
-    //                 if (wrapper.equals(plugPagId)) {
-    //                     plugPagWrapper = wrapper;
-    //                     break;
-    //                 }
-    //             }
+                for (PlugPagWrapper wrapper: plugPags) {
+                    if (wrapper.equals(plugPagId)) {
+                        plugPagWrapper = wrapper;
+                        break;
+                    }
+                }
 
-    //             if (plugPagWrapper != null) {
-    //                 String[] installments = plugPagWrapper.plugPag.calculateInstallments(saleValue);
-    //                 successCallback.invoke(installments);
-    //             } else {
-    //                 errorCallback.invoke("Can't find plugPag");
-    //             }
-    //         } else {
-    //             errorCallback.invoke("PlugPagSaleValue error");
-    //         }
-    //     } catch (Exception err) {
-    //         errorCallback.invoke("Can't calculate installments: " + err.toString());
-    //     }
-    // }
+                if (plugPagWrapper != null) {
+                    String[] installments = plugPagWrapper.plugPag.calculateInstallments(saleValue);
+                    successCallback.invoke(installments);
+                } else {
+                    errorCallback.invoke("Can't find plugPag");
+                }
+            } else {
+                errorCallback.invoke("PlugPagSaleValue error");
+            }
+        } catch (Exception err) {
+            errorCallback.invoke("Can't calculate installments: " + err.toString());
+        }
+    }
 
-    // @ReactMethod
-    // public void readCard(String plugPagId, Callback successCallback, Callback errorCallback) {
-    //     try {
-    //         PlugPagWrapper plugPagWrapper = null;
+    @ReactMethod
+    public void readCard(String plugPagId, Callback successCallback, Callback errorCallback) {
+        try {
+            PlugPagWrapper plugPagWrapper = null;
 
-    //         for (PlugPagWrapper wrapper: plugPags) {
-    //             if (wrapper.equals(plugPagId)) {
-    //                 plugPagWrapper = wrapper;
-    //                 break;
-    //             }
-    //         }
+            for (PlugPagWrapper wrapper: plugPags) {
+                if (wrapper.equals(plugPagId)) {
+                    plugPagWrapper = wrapper;
+                    break;
+                }
+            }
 
-    //         if (plugPagWrapper != null) {
-    //             PlugPagCardInfoResult dataCard = plugPagWrapper.plugPag.getCardData();
-    //             successCallback.invoke(dataCard.getResult());
-    //         } else {
-    //             errorCallback.invoke("Can't find plugPag");
-    //         }
-    //     } catch (Exception err) {
-    //         errorCallback.invoke("Can't read card: " + err.toString());
-    //     }
-    // }
+            if (plugPagWrapper != null) {
+                PlugPagCardInfoResult dataCard = plugPagWrapper.plugPag.getCardData();
+                successCallback.invoke(dataCard.getResult());
+            } else {
+                errorCallback.invoke("Can't find plugPag");
+            }
+        } catch (Exception err) {
+            errorCallback.invoke("Can't read card: " + err.toString());
+        }
+    }
 
-    // @ReactMethod
-    // public void readNFCCard(String plugPagId, Callback successCallback, Callback errorCallback) {
-    //     try {
-    //         PlugPagWrapper plugPagWrapper = null;
+    @ReactMethod
+    public void readNFCCard(String plugPagId, Callback successCallback, Callback errorCallback) {
+        try {
+            PlugPagWrapper plugPagWrapper = null;
 
-    //         for (PlugPagWrapper wrapper: plugPags) {
-    //             if (wrapper.equals(plugPagId)) {
-    //                 plugPagWrapper = wrapper;
-    //                 break;
-    //             }
-    //         }
+            for (PlugPagWrapper wrapper: plugPags) {
+                if (wrapper.equals(plugPagId)) {
+                    plugPagWrapper = wrapper;
+                    break;
+                }
+            }
 
-    //         if (plugPagWrapper != null) {
-    //             PlugPagNearFieldCardData dataCard = new PlugPagNearFieldCardData();
-    //             dataCard.setStartSlot(1);
-    //             dataCard.setEndSlot(1);
+            if (plugPagWrapper != null) {
+                PlugPagNearFieldCardData dataCard = new PlugPagNearFieldCardData();
+                dataCard.setStartSlot(1);
+                dataCard.setEndSlot(1);
 
-    //             PlugPagNFCResult result = plugPagWrapper.plugPag.readFromNFCCard(dataCard);
-    //             successCallback.invoke(result.getResult());
-    //         } else {
-    //             errorCallback.invoke("Can't find plugPag");
-    //         }
-    //     } catch (Exception err) {
-    //         errorCallback.invoke("Can't read NFC card: " + err.toString());
-    //     }
-    // }
+                PlugPagNFCResult result = plugPagWrapper.plugPag.readFromNFCCard(dataCard);
+                successCallback.invoke(result.getResult());
+            } else {
+                errorCallback.invoke("Can't find plugPag");
+            }
+        } catch (Exception err) {
+            errorCallback.invoke("Can't read NFC card: " + err.toString());
+        }
+    }
 
 }
